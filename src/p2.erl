1f6eb25b0f4033ccc239bdfe942a796bab0ddb86
Anthony Valentino <97629772+ajvalentino@users.noreply.github.com>
2024-09-16 11:30:21 -0400

    Completed part 2

--- src/p2.erl
+++ src/p2.erl
@@ -5,8 +5,15 @@
 -author("Lauren Ferlito and AJ Valentino").
 
 %% API
--export([hello_world/0]).
+-export([loop/0]).
 
-%% Function to print Hello, World!
-hello_world() ->
-  io:format("Hello, World!~n").
+%% Recursive loop function
+loop() ->
+    p1:start(),  % Call the computation function from p1
+    Input = io:get_line("Press enter to continue or '0' to quit: "),
+    case string:trim(Input) of
+        "0" -> 
+            io:format("Exiting program.~n");
+        _ ->
+            loop()  % Tail recursion to continue looping
+    end.
